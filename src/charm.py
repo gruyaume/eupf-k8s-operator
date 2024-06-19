f4daf97eadf9d4d08be0371b9a0dfb63078f2978
Guillaume Belanger <guillaume.belanger27@gmail.com>
2024-06-19 07:21:55 -0400

    feat: add correct routes (#26)

--- src/charm.py
+++ src/charm.py
@@ -27,10 +27,11 @@ from ops import RemoveEvent
 from ops.charm import CharmEvents, CollectStatusEvent
 from ops.framework import EventBase, EventSource
 from ops.model import ActiveStatus, ModelError, WaitingStatus
-from ops.pebble import ConnectionError, ExecError, Layer
+from ops.pebble import ConnectionError, ExecError, Layer, PathError
 
 logger = logging.getLogger(__name__)
 
+UE_SUBNET = "172.250.1.0/16"
 CONFIG_FILE_NAME = "config.yaml"
 CONFIG_PATH = "/etc/eupf"
 PFCP_PORT = 8805
@@ -165,18 +166,10 @@ class EupfK8SOperatorCharm(ops.CharmBase):
             self._pfcp_service.create()
         if not self._ebpf_volume.is_created():
             self._ebpf_volume.create()
-        if not self._route_exists(
-            dst="default",
-            via=str(self._charm_config.n6_gateway_ip),
-        ):
-            self._create_default_route()
-        if not self._route_exists(
-            dst=str(self._charm_config.gnb_subnet),
-            via=str(self._charm_config.n3_gateway_ip),
-        ):
-            self._create_ran_route()
-        if not self._ip_tables_rule_exists():
-            self._create_ip_tables_rule()
+        self._accept_forwarded_packets()
+        self._add_routing_table()
+        self._create_ip_rule()
+        self._create_default_route()
         restart = self._generate_config_file()
         self._configure_pebble(restart=restart)
         self._update_fiveg_n4_relation_data()
@@ -189,26 +182,65 @@ class EupfK8SOperatorCharm(ops.CharmBase):
         if self._pfcp_service.is_created():
             self._pfcp_service.delete()
 
-    def _ip_tables_rule_exists(self) -> bool:
-        """Return whether iptables rule already exists using the `--check` parameter."""
+    def _accept_forwarded_packets(self) -> None:
+        """Configure packet filtering rules to accept all forwarded packets."""
         try:
             self._exec_command_in_workload(
-                command="iptables-legacy --check OUTPUT -p icmp --icmp-type port-unreachable -j DROP"  # noqa: E501
-            )
-            return True
-        except ExecError:
-            return False
-
-    def _create_ip_tables_rule(self) -> None:
-        """Create iptable rule in the OUTPUT chain to block ICMP port-unreachable packets."""
-        try:
-            self._exec_command_in_workload(
-                command="iptables-legacy -I OUTPUT -p icmp --icmp-type port-unreachable -j DROP"
+                command="iptables-legacy -A FORWARD -j ACCEPT"
             )
         except ExecError as e:
-            logger.error("Failed to create iptables rule for ICMP: %s", e.stderr)
+            logger.error("Failed to run iptables command: %s", e.stderr)
             return
-        logger.info("Iptables rule for ICMP created")
+        except FileNotFoundError:
+            logger.error("Failed to execute command. File not found.")
+            return
+        logger.info("Iptables command ran successfully")
+
+    def _add_routing_table(self) -> None:
+        """Add a routing table named n6if with ID 1200."""
+        try:
+            existing_rt_tables = self._container.pull(path="/etc/iproute2/rt_tables")
+        except PathError:
+            logger.error("Failed to pull existing routing tables")
+            return
+        existing_rt_tables_str = existing_rt_tables.read()
+        if "1200 n6if" in existing_rt_tables_str:
+            logger.info("Routing table already exists")
+            return
+        concatenate = f"{existing_rt_tables_str}\n1200 n6if\n"
+        try:
+            self._container.push(path="/etc/iproute2/rt_tables", source=concatenate)
+            logger.info("Pushed routing table")
+        except ConnectionError:
+            logger.error("Failed to push routing table")
+            return
+
+    def _create_ip_rule(self) -> None:
+        """Add a rule to use the n6if table for traffic from the User Equipment subnet."""
+        try:
+            self._exec_command_in_workload(
+                command=f"ip rule add from {UE_SUBNET} table n6if"
+            )
+        except ExecError as e:
+            logger.error("Failed to add ip rule: %s", e.stderr)
+            return
+        except FileNotFoundError:
+            logger.error("Failed to execute command. File not found.")
+            return
+
+    def _create_default_route(self) -> None:
+        """Add a default route in the n6if table via the N6 Gateway and N6 network interface."""
+        try:
+            self._exec_command_in_workload(
+                command=f"ip route add default via {self._charm_config.n6_gateway_ip} dev {N6_INTERFACE_NAME} table n6if"
+            )
+        except ExecError as e:
+            logger.error("Failed to add default route: %s", e.stderr)
+            return
+        except FileNotFoundError:
+            logger.error("Failed to execute command. File not found.")
+            return
+        logger.info("Default route added successfully")
 
     def _exec_command_in_workload(
         self, command: str, timeout: Optional[int] = 30, environment: Optional[dict] = None
@@ -219,49 +251,6 @@ class EupfK8SOperatorCharm(ops.CharmBase):
             environment=environment,
         )
         return process.wait_output()
-
-    def _route_exists(self, dst: str, via: str | None) -> bool:
-        """Return whether the specified route exist."""
-        try:
-            stdout, _ = self._exec_command_in_workload(command="ip route show")
-        except ExecError as e:
-            logger.error("Failed retrieving routes: %s", e.stderr)
-            return False
-        except FileNotFoundError:
-            logger.error("Failed to execute command. File not found.")
-            return False
-        for line in stdout.splitlines():
-            if f"{dst} via {via}" in line:
-                return True
-        return False
-
-    def _create_default_route(self) -> None:
-        """Create ip route towards core network."""
-        try:
-            self._exec_command_in_workload(
-                command=f"ip route replace default via {self._charm_config.n6_gateway_ip} metric 110"
-            )
-        except ExecError as e:
-            logger.error("Failed to create core network route: %s", e.stderr)
-            return
-        except FileNotFoundError:
-            logger.error("Failed to execute command. File not found.")
-            return
-        logger.info("Default core network route created")
-
-    def _create_ran_route(self) -> None:
-        """Create ip route towards gnb-subnet."""
-        try:
-            self._exec_command_in_workload(
-                command=f"ip route replace {self._charm_config.gnb_subnet} via {str(self._charm_config.n3_gateway_ip)}"
-            )
-        except ExecError as e:
-            logger.error("Failed to create route to gnb-subnet: %s", e.stderr)
-            return
-        except FileNotFoundError:
-            logger.error("Failed to execute command. File not found.")
-            return
-        logger.info("Route to gnb-subnet created")
 
     def _eupf_service_is_running(self) -> bool:
         try:
@@ -389,11 +378,11 @@ class EupfK8SOperatorCharm(ops.CharmBase):
             name=N3_NETWORK_ATTACHMENT_DEFINITION_NAME,
             interface=N3_INTERFACE_NAME,
         )
-        n4_network_annotation = NetworkAnnotation(
+        n6_network_annotation = NetworkAnnotation(
             name=N6_NETWORK_ATTACHMENT_DEFINITION_NAME,
             interface=N6_INTERFACE_NAME,
         )
-        return [n3_network_annotation, n4_network_annotation]
+        return [n3_network_annotation, n6_network_annotation]
 
     def _network_attachment_definitions_from_config(self) -> list[NetworkAttachmentDefinition]:
         n3_nad_config= {
@@ -408,7 +397,7 @@ class EupfK8SOperatorCharm(ops.CharmBase):
             "type": "bridge",
             "bridge": N3_INTERFACE_BRIDGE_NAME
         }
-        n4_nad_config = {
+        n6_nad_config = {
             "cniVersion": "0.3.1",
             "ipam": {
                 "type": "static",
@@ -429,15 +418,15 @@ class EupfK8SOperatorCharm(ops.CharmBase):
             ),
             spec={"config": json.dumps(n3_nad_config)},
         )
-        n4_nad = NetworkAttachmentDefinition(
+        n6_nad = NetworkAttachmentDefinition(
             metadata=ObjectMeta(
                 name=(
                     N6_NETWORK_ATTACHMENT_DEFINITION_NAME
                 )
             ),
-            spec={"config": json.dumps(n4_nad_config)},
+            spec={"config": json.dumps(n6_nad_config)},
         )
-        return [n3_nad, n4_nad]
+        return [n3_nad, n6_nad]
 
 
 
